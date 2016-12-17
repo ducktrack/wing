34236a930414389fe37327f9d6dbbe67adfe46c6
Thiago Rodrigues Colucci <t.rodrigues-colucci@klarna.com>
2016-12-17 18:04:41 +0100

    strip script tags from incoming HTML

--- exporters/file_exporter.go
+++ exporters/file_exporter.go
@@ -5,9 +5,11 @@ import (
 	"errors"
 	"fmt"
 	"github.com/duckclick/wing/config"
+	"github.com/PuerkitoBio/goquery"
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 type FileExporter struct {
@@ -20,11 +22,24 @@ func (fe *FileExporter) Export(trackEntry *TrackEntry, recordId string) error {
 		return errors.New("Invalid base64 payload")
 	}
 
+	doc, err := goquery.NewDocumentFromReader(strings.NewReader(string(htmlBytes)))
+	if err != nil {
+		return errors.New("Failed to parse HTML")
+	}
+
+	scripts := doc.Find("script")
+	scripts.Each(func(i int, s *goquery.Selection) { s.Remove() })
+
+	content, err := doc.Html()
+	if err != nil {
+		return errors.New("Failed to generate secure HTML")
+	}
+
 	recordPath := filepath.Join(fe.Config.Folder, recordId)
 	os.MkdirAll(recordPath, os.ModePerm)
 
 	fileName := filepath.Join(recordPath, fmt.Sprintf("%d.html", trackEntry.CreatedAt))
-	err = ioutil.WriteFile(fileName, htmlBytes, 0644)
+	err = ioutil.WriteFile(fileName, []byte(content), 0644)
 	if err != nil {
 		return errors.New(fmt.Sprintf("Failed to save track entry to '%s'", fileName))
 	}
--- exporters/file_exporter_test.go
+++ exporters/file_exporter_test.go
@@ -8,6 +8,7 @@ import (
 	"io/ioutil"
 	"os"
 	"testing"
+	"encoding/base64"
 )
 
 func TestExport(t *testing.T) {
@@ -15,9 +16,12 @@ func TestExport(t *testing.T) {
 		Folder: "/tmp/test/track_entries",
 	}
 
+	htmlSample := "<html><head></head><body></body></html>"
+	htmlAsBase64 := base64.StdEncoding.EncodeToString([]byte(htmlSample))
+
 	trackEntry := &TrackEntry{
 		CreatedAt: 123456,
-		Markup:    "PGh0bWw+PC9odG1sPg==",
+		Markup:    htmlAsBase64,
 	}
 
 	recordId := uuid.NewV4().String()
@@ -34,5 +38,34 @@ func TestExport(t *testing.T) {
 	defer os.Remove(recordPath)
 
 	htmlBytes, _ := ioutil.ReadFile(recordPath)
-	assert.Equal(t, "<html></html>", string(htmlBytes), "FileExporter#Export should save the expected content")
+	assert.Equal(t, "<html><head></head><body></body></html>", string(htmlBytes), "FileExporter#Export should save the expected content")
+}
+
+func TestStripScriptTags(t *testing.T) {
+	exporterConfig := config.FileExporter{
+		Folder: "/tmp/test/track_entries",
+	}
+	htmlSample := `<html><head><script src="evil"></script></head><body><script src="g-evil"></script></body></html>`
+	htmlAsBase64 := base64.StdEncoding.EncodeToString([]byte(htmlSample))
+
+	trackEntry := &TrackEntry{
+		CreatedAt: 123456,
+		Markup:    htmlAsBase64,
+	}
+
+	recordId := uuid.NewV4().String()
+
+	exporter := FileExporter{Config: exporterConfig}
+	err := exporter.Export(trackEntry, recordId)
+	assert.Nil(t, err, "export should succeed")
+
+	recordPath := fmt.Sprintf("/tmp/test/track_entries/%s/%d.html", recordId, trackEntry.CreatedAt)
+	if _, err := os.Stat(recordPath); os.IsNotExist(err) {
+		fmt.Sprintf("FileExporter#Export failed to save track entry, expected:\n%v\n to exist", recordPath)
+		t.FailNow()
+	}
+	defer os.Remove(recordPath)
+
+	htmlBytes, _ := ioutil.ReadFile(recordPath)
+	assert.Equal(t, "<html><head></head><body></body></html>", string(htmlBytes), "FileExporter#Export should save the expected content")
 }
