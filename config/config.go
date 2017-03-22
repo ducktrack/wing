bd07fa31b0af8c4a2022cf642fcbd010d0ed595f
Túlio Ornelas <tulio.ornelas@klarna.com>
2017-03-22 19:39:44 +0100

    run golint in config/config

--- config/config.go
+++ config/config.go
@@ -1,29 +1,33 @@
 package config
 
 import (
+	"github.com/pkg/errors"
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
 	"os"
-	"github.com/pkg/errors"
 )
 
+// Config definition
 type Config struct {
-	TLSCertFile   string        `yaml:"tls_cert_file"`
-	TLSKeyFile    string        `yaml:"tls_key_file"`
+	TLSCertFile   string `yaml:"tls_cert_file"`
+	TLSKeyFile    string `yaml:"tls_key_file"`
 	Exporter      string
 	FileExporter  FileExporter  `yaml:"file_exporter,omitempty"`
 	RedisExporter RedisExporter `yaml:"redis_exporter,omitempty"`
 }
 
+// FileExporter definition
 type FileExporter struct {
 	Folder string `yaml:"folder"`
 }
 
+// RedisExporter definition
 type RedisExporter struct {
-	Host     string `yaml:"host"`
-	Port     int16  `yaml:"port"`
+	Host string `yaml:"host"`
+	Port int16  `yaml:"port"`
 }
 
+// ReadConfigFile reads and parses the configuration file (application.yml)
 func ReadConfigFile(path string) (*Config, error) {
 	if _, err := os.Stat(path); os.IsNotExist(err) {
 		return nil, errors.Errorf("The configuration file is missing, expected file '%s'", path)
